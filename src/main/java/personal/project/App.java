5df24d524dba90b524096103e891eb1a908068a5
Kelvin Chung <kelvinc2016@gmail.com>
2021-03-19 12:16:48 -0700

    Initial commit

---
+++ src/main/java/personal/project/App.java
@@ -0,0 +1,16 @@
+package personal.project;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        MaxHeap maxHeap = new MaxHeap();
+        System.out.println(maxHeap.peekMax());
+    }
+}
---
+++ src/main/java/personal/project/MaxHeap.java
@@ -0,0 +1,27 @@
+package personal.project;
+
+public class MaxHeap {
+  private int[] heap;
+  public MaxHeap() {
+    this.heap = new int[11];
+  }
+
+  // Initializes MaxHeap to contain [values]
+  // Params:
+  //    - int[] values: initial values that heap contains
+  public MaxHeap(int[] values) {
+    this.heap = new int[values.length];
+  }
+
+  public void insert(int value) {
+
+  }
+
+  public int deleteMax() {
+    return -1;
+  }
+
+  public int peekMax() {
+    return -1;
+  }
+}
